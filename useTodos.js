8af23a46cd9c5d9defe185c96579acfd45bda827
Reinaldo Romero <reinaldo.romero@pjchile.com>
2022-11-10 20:38:38 -0300

    First commit

---
+++ useTodos.js
@@ -0,0 +1,59 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+  // {
+  //   id: new Date().getTime(),
+  //   description: 'Recolectar la piedra del alma',
+  //   done: false,
+  // },
+
+const init = () => {
+return JSON.parse( localStorage.getItem( 'todos' )) || [];
+}
+
+export const useTodos = () => {
+
+  const [todos, dispatch] = useReducer( todoReducer, [], init)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify( todos ) );
+
+  }, [todos])
+
+
+  const handleNewTodo = ( todo ) => {
+    const action = {
+      type: '[TODO] Add ToDo',
+      payload: todo
+    }
+    dispatch( action );
+  }
+
+  const handleDeleteTodo = ( id ) => {
+    dispatch({
+      type: '[TODO] Remove ToDo',
+      payload: id
+    });
+  }
+
+  const handleToggleTodo = ( id ) => {
+   // console.log({ id })
+    dispatch({
+      type: '[TODO] Toggle ToDo',
+      payload: id
+    });
+  }
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter(todo=> !todo.done).length,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo
+  }
+
+
+
+}
